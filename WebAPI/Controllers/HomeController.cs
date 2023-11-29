9c48702e318b9362a3f87e3485044302b31415ac
AswinKumar77 <114729030+AswinKumar77@users.noreply.github.com>
2023-11-29 09:21:01 +0530

    Add files via upload

---
+++ WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase 
+    {
+     
+        private readonly ApplicationDbContext _dbcontext;
+
+        public HomeController(ApplicationDbContext _dbcontext)
+        {
+           this._dbcontext = _dbcontext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<PlayerModel>> Get()
+        {
+            return _dbcontext.PlayerDetails.ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<PlayerModel> Create([FromBody]PlayerModel model)
+        {
+            _dbcontext.PlayerDetails.Add(model);
+            _dbcontext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Models/PlayerModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class PlayerModel
+    {
+        
+        public int Id { get; set; }
+       
+        public string PlayerName { get; set; }
+       
+        public string CountryName { get; set; }
+   
+        public int Age {  get; set; }
+    }
+}
